0affbf59185747c372d8c5261ca14ef47237c89c
KalopsiaZom <bryant.marchianno@gmail.com>
2025-10-18 09:21:51 +0700

    env

--- koneksi.js
+++ koneksi.js
@@ -1,22 +1,22 @@
-const mysql = require('mysql2')
+require('dotenv').config();
+const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST, 
-    user: process.env.DB_USERNAME, 
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DBNAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 pool.getConnection((err, conn) => {
-    if (err) console.error("MySQL connection failed:", err);
-    else {
-        console.log("Connected successfully");
-        conn.release(); 
-    }
+  if (err) console.error("MySQL connection failed:", err);
+  else {
+    console.log("Connected successfully");
+    conn.release();
+  }
 });
 
-
-module.exports = pool.promise()
+module.exports = pool.promise();
